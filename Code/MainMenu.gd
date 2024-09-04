e36ea5f1356112536f265c4499a5f0793a4f7c87
CoolCoderMan281 <smitmic.erm@gmail.com>
2024-09-03 19:27:26 -0700

    Settings (Functionality)
    What works for sure:
    
    - Sizing things
    
    What is untested but probably works:
    
    - Volume control

--- Code/MainMenu.gd
+++ Code/MainMenu.gd
@@ -1,6 +1,10 @@
 extends Node
 
 
+func _ready():
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+
+
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
@@ -15,3 +19,19 @@ func _on_back_pressed():
 
 func _on_settings_to_pressed():
 	%Settings.show()
+
+
+func _on_volume_value_changed(value:float):
+	%Volume_Label.text = "Volume: " + str(value) + "%"
+	AudioServer.set_bus_volume_db(0, value)
+
+func _on_windowed_pressed():
+	%Windowed.disabled = true
+	%Fullscreen.disabled = false
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+
+	
+func _on_fullscreen_pressed():
+	%Windowed.disabled = false
+	%Fullscreen.disabled = true
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
