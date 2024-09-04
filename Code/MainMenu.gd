b6e839c2cce38af8129bb68ede69d58ea856aa19
CoolCoderMan281 <smitmic.erm@gmail.com>
2024-09-03 19:13:13 -0700

    Settings menu (Layout)
    (Not implemented, just the layout)

--- Code/MainMenu.gd
+++ Code/MainMenu.gd
@@ -4,5 +4,14 @@ extends Node
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
+
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_back_pressed():
+	%Settings.hide()
+
+
+func _on_settings_to_pressed():
+	%Settings.show()
