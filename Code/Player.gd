f9484c0891a632dc9083bd9363404b89e65e428c
CoolCoderMan281 <smitmic.erm@gmail.com>
2024-09-17 12:40:49 -0700

    Button press sprite change

--- Code/Player.gd
+++ Code/Player.gd
@@ -1,13 +1,27 @@
 extends Node2D
 
 var score: int = 0
-
+var pressed_timer: Timer
 
 func _ready() -> void:
 	score = 0
 	update_score()
 	update_selection_images()
+	pressed_timer = Timer.new()
+	pressed_timer.set_wait_time(0.5)
+	pressed_timer.one_shot = true
+	add_child(pressed_timer)
+	pressed_timer.connect("timeout", Callable(self,"update_selection_images"))
 
+func visual_pressed(button: String):
+	if button == "Rock":
+		%Rock.texture_normal = load(Globals.resource_path+"Buttons/Rock/Center/pressed_rock_center.png")
+	elif button == "Paper":
+		%Paper.texture_normal = load(Globals.resource_path+"Buttons/Paper/Center/pressed_paper_center.png")
+	elif button == "Scissors":
+		%Scissors.texture_normal = load(Globals.resource_path+"Buttons/Scissors/Center/pressed_scissors_center.png")
+
+	pressed_timer.start()
 
 func logic(choice: String = ""):
 	var outcomes = {"Rock": "Scissors", "Paper": "Rock", "Scissors": "Paper"}
@@ -29,9 +43,9 @@ func logic(choice: String = ""):
 
 
 func update_selection_images():
-	%Rock.texture_normal = load(Globals.resource_path+"rock_button.png")
-	%Paper.texture_normal = load(Globals.resource_path+"paper_button.png")
-	%Scissors.texture_normal = load(Globals.resource_path+"scissors_button.png")
+	%Rock.texture_normal = load(Globals.resource_path+"Buttons/Rock/Center/rock_center.png")
+	%Paper.texture_normal = load(Globals.resource_path+"Buttons/Paper/Center/paper_center.png")
+	%Scissors.texture_normal = load(Globals.resource_path+"Buttons/Scissors/Center/scissors_center.png")
 
 
 func update_score():
@@ -41,14 +55,17 @@ func update_score():
 
 func _on_rock_pressed():
 	logic("Rock")
+	visual_pressed("Rock")
 
 
 func _on_paper_pressed():
 	logic("Paper")
+	visual_pressed("Paper")
 
 
 func _on_scissors_pressed():
 	logic("Scissors")
+	visual_pressed("Scissors")
 
 
 func retry():
