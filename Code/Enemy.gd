32fbd3f6f9b791623fdb89d42249508a04449d4c
CoolCoderMan281 <smitmic.erm@gmail.com>
2024-09-24 11:06:40 -0700

    Transitions Out

--- Code/Enemy.gd
+++ Code/Enemy.gd
@@ -2,8 +2,8 @@ extends Sprite2D
 
 
 var options: Array[String] = ["Rock", "Paper", "Scissors"]
-var choice: String
-var pastChoice: String
+var choice: String = options.pick_random()
+var pastChoice: String = options.pick_random()
 
 
 const base_path: String = "res://Assets/Default/Conveyor/"
@@ -44,6 +44,34 @@ var preloaded_textures: Dictionary = {
 }
 
 
+var preloaded_textures_out: Dictionary = {
+	"Rock": [
+		preload(base_path + "Rock/rock_out_1.png"),
+		preload(base_path + "Rock/rock_out_2.png"),
+		preload(base_path + "Rock/rock_out_3.png"),
+		preload(base_path + "Rock/rock_out_4.png"),
+		preload(base_path + "Rock/rock_out_5.png"),
+		preload(base_path + "Rock/rock_out_6.png")
+	],
+	"Paper": [
+		preload(base_path + "Paper/paper_out_1.png"),
+		preload(base_path + "Paper/paper_out_2.png"),
+		preload(base_path + "Paper/paper_out_3.png"),
+		preload(base_path + "Paper/paper_out_4.png"),
+		preload(base_path + "Paper/paper_out_5.png"),
+		preload(base_path + "Paper/paper_out_6.png")
+	],
+	"Scissors": [
+		preload(base_path + "Scissors/scissors_out_1.png"),
+		preload(base_path + "Scissors/scissors_out_2.png"),
+		preload(base_path + "Scissors/scissors_out_3.png"),
+		preload(base_path + "Scissors/scissors_out_4.png"),
+		preload(base_path + "Scissors/scissors_out_5.png"),
+		preload(base_path + "Scissors/scissors_out_6.png")
+	]
+}
+
+
 var fps: int = 12  # Rayan using 12 fps for some reason :skull:
 
 func _ready():
@@ -51,16 +79,26 @@ func _ready():
 
 
 func update_display() -> void:
-	texture = transition_texture
 	await get_tree().create_timer(0.2).timeout
+	await play_leave_animation()
+	texture = transition_texture
 	await play_animation_frames()
 	texture = preloaded_textures[choice].back()
+
+
+func play_leave_animation() -> void:
+	var total_frames = 6
+	var frame_delay = 1.0 / fps
+
+	for i in range(1,6):
+		print(pastChoice+"_out_"+str(i))
+		texture = preloaded_textures_out[pastChoice][i]
+		await get_tree().create_timer(frame_delay).timeout
 
 
 func play_animation_frames() -> void:
 	var total_frames = frame_counts.get(choice)
 	var frame_delay = 1.0 / fps
-	
 
 	for i in range(total_frames):
 		texture = preloaded_textures[choice][i]
@@ -69,10 +107,9 @@ func play_animation_frames() -> void:
 
 func new_choice() -> void:
 	var possible_choices = options.duplicate()
+	pastChoice = choice
 	while true:
 		choice = possible_choices.pick_random()
 		if choice != pastChoice:
 			break
-
-	pastChoice = choice
 	update_display()
