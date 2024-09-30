516d45f9ce92e2c01bebb7b4d353908bfc3bfb01
CoolCoderMan281 <smitmic.erm@gmail.com>
2024-09-30 11:13:56 -0700

    Lizard - Hunter MVP

--- Code/Player.gd
+++ Code/Player.gd
@@ -13,6 +13,7 @@ func _ready() -> void:
 	add_child(pressed_timer)
 	pressed_timer.connect("timeout", Callable(self,"update_selection_images"))
 
+
 func visual_pressed(button: String):
 	if button == "Rock":
 		%Rock.texture_normal = load(Globals.resource_path+"Buttons/Rock/Center/pressed_rock_center.png")
@@ -20,32 +21,50 @@ func visual_pressed(button: String):
 		%Paper.texture_normal = load(Globals.resource_path+"Buttons/Paper/Center/pressed_paper_center.png")
 	elif button == "Scissors":
 		%Scissors.texture_normal = load(Globals.resource_path+"Buttons/Scissors/Center/pressed_scissors_center.png")
+	elif button == "Lizard":
+		%Lizard.texture_normal = load(Globals.resource_path+"Buttons/Lizard/Center/pressed_lizard_center.png")
+	elif button == "Hunter":
+		%Hunter.texture_normal = load(Globals.resource_path+"Buttons/Hunter/Center/pressed_hunter_center.png")
 
 	pressed_timer.start()
 
+
 func logic(choice: String = ""):
-	var outcomes = {"Rock": "Scissors", "Paper": "Rock", "Scissors": "Paper"}
-	var enemy_choice = %Enemy.choice
+	var outcomes = {
+		"Rock": ["Scissors", "Lizard"],
+		"Scissors": ["Paper", "Lizard"],
+		"Paper": ["Rock", "Hunter"],
+		"Lizard": ["Hunter", "Paper"],
+		"Hunter": ["Scissors", "Rock"]
+	}
 	
+	var enemy_choice = %Enemy.choice
+
 	if choice != "":
-		if outcomes[choice] == enemy_choice:
+		if choice in outcomes and enemy_choice in outcomes[choice]:
 			score += 1
 			%Enemy.new_choice()
 			%Countdown.reset_timer()
-		elif outcomes[enemy_choice] == choice:
+		elif enemy_choice in outcomes and choice in outcomes[enemy_choice]:
 			%Countdown.end_game("lose")
+		else:
+			# If there's a tie
+			%Countdown.reset_timer()
 	else:
 		score += 1
 		%Enemy.new_choice()
 		%Countdown.reset_timer()
-	
+
 	update_score()
+
 
 
 func update_selection_images():
 	%Rock.texture_normal = load(Globals.resource_path+"Buttons/Rock/Center/rock_center.png")
 	%Paper.texture_normal = load(Globals.resource_path+"Buttons/Paper/Center/paper_center.png")
 	%Scissors.texture_normal = load(Globals.resource_path+"Buttons/Scissors/Center/scissors_center.png")
+	%Lizard.texture_normal = load(Globals.resource_path+"Buttons/Lizard/Center/lizard_center.png")
+	%Hunter.texture_normal = load(Globals.resource_path+"Buttons/Hunter/Center/hunter_center.png")
 
 
 func update_score():
@@ -66,6 +85,16 @@ func _on_paper_pressed():
 func _on_scissors_pressed():
 	logic("Scissors")
 	visual_pressed("Scissors")
+
+
+func _on_hunter_pressed():
+	logic("Hunter")
+	visual_pressed("Hunter")
+
+
+func _on_lizard_pressed():
+	logic("Lizard")
+	visual_pressed("Lizard")
 
 
 func retry():
